b4cc7d2c0a655f0f9c4b8ba6d77527366458af62
TinFoiKa <117747442+TinFoiKa@users.noreply.github.com>
2024-09-08 16:36:45 +0800

    Rearranged Login

--- src/forms/Login.tsx
+++ src/forms/Login.tsx
@@ -1,6 +1,25 @@
+import "./Login.css"
+
 const Login = () => {
     return (
-        <></>
+        <>
+            <div className="route" id="welcome"></div>
+                <div id="app">
+                <div className="app-view">  
+                    <header className="app-header">
+                    <h1>Lines</h1>
+                    Welcome back,<br/>
+                    <span className="app-subheading">
+                        sign in to continue<br/>
+                        to Lines.
+                    </span>
+                    </header>
+                    <input type="email" required pattern=".*\.\w{2,}" placeholder="Email Address" />
+                    <input type="password" required placeholder="Password" />
+                    <a href="#welcome" className="app-button">Login</a>
+                </div>
+            </div>
+        </>
     )
 }
 
